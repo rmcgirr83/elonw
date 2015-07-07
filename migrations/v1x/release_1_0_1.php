f62ae3268b7619fae21fb85b6f913f06b6a7298b
RMcGirr83 <rmcgirr83@gmail.com>
2015-07-07 11:20:00 -0400

    Add fontawesome

--- migrations/v1x/release_1_0_1.php
+++ migrations/v1x/release_1_0_1.php
@@ -2,36 +2,29 @@
 /**
 *
 * @package External Links Open in New Window
-* @copyright (c) 2014 RMcGirr83
+* @copyright (c) 2015 RMcGirr83
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
 namespace rmcgirr83\elonw\migrations\v1x;
 
-class release_1_0_1_data extends \phpbb\db\migration\migration
+class release_1_0_1 extends \phpbb\db\migration\migration
 {
 	public function effectively_installed()
 	{
 		return isset($this->config['elonw_version']) && version_compare($this->config['elonw_version'], '1.0.1', '>=');
 	}
-
+	
 	static public function depends_on()
 	{
-		return array('\rmcgirr83\elonw\migrations\v1x\release_1_0_0_data');
-	}
+		return array('\rmcgirr83\elonw\migrations\release_1_0_0_data.php');
+	}	
 
 	public function update_data()
 	{
 		return array(
-			array('config.add', array('elonw_version', '1.0.1')),
-		);
-	}
-
-	public function revert_data()
-	{
-		return array(
-			array('config.remove', array('elonw_version')),
+			array('config.update', array('elonw_version', '1.0.1')),
 		);
 	}
 }
